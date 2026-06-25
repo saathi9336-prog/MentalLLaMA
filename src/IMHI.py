db05a221fc3a04ba72785b35bac7bc4878aba2da
saathi9336-prog <saathi9336@gmail.com>
2026-06-25 17:46:25 +0530

    Update IMHI.py

--- src/IMHI.py
+++ src/IMHI.py
@@ -3,7 +3,13 @@ import argparse
 import pandas as pd
 
 import torch
-from transformers import AutoModel, AutoTokenizer, LlamaForCausalLM, LlamaTokenizer
+from transformers import (
+    AutoModel,
+    AutoTokenizer,
+    LlamaForCausalLM,
+    LlamaTokenizer,
+    BitsAndBytesConfig
+)
 from peft import PeftModel
 
 from sklearn.metrics import f1_score, confusion_matrix, accuracy_score, classification_report, \
@@ -43,7 +49,7 @@ def generate_response(model, tokenizer, test_data, device, batch_size):
     generated_text = {}
     goldens = {}
 
-    model.to(device)
+    
 
     for dataset_name in test_data.keys():
         #if dataset_name not in ['DR', 'dreaddit']:
@@ -55,14 +61,21 @@ def generate_response(model, tokenizer, test_data, device, batch_size):
 
         for i in range(0, len(queries), batch_size):
             batch_data = queries[i: min(i+batch_size, len(queries))]
-            #print(batch_data[:2])
-            inputs = tokenizer(batch_data, return_tensors="pt", padding=True)
-            #print(inputs)
-            #final_input = inputs.input_ids
-            input_ids = inputs.input_ids.to(device)
-            attention_mask = inputs.attention_mask.to(device)
+            inputs = tokenizer(
+                batch_data,
+                return_tensors="pt",
+                padding=True
+            ).to(model.device)
+
+            input_ids = inputs.input_ids
+            attention_mask = inputs.attention_mask
             #print(final_input)
-            generate_ids = model.generate(input_ids, attention_mask=attention_mask, max_length=2048)
+            generate_ids = model.generate(
+                input_ids,
+                attention_mask=attention_mask,
+                max_new_tokens=256,
+                do_sample=False
+            )
             for j in range(generate_ids.shape[0]):
                 truc_ids = generate_ids[j][len(input_ids[j]) :]
                 response = tokenizer.decode(truc_ids, skip_special_tokens=True, spaces_between_special_tokens=False)
@@ -253,11 +266,28 @@ def calculate_f1(generated, goldens, output_path):
             f.write(result)
         print(count)
 
-def main(model_path: str, model_output_path: str, batch_size: int, test_dataset: str, rule_calculate: bool,
-         llama: bool, device: str, lora: bool, cuda: bool, lora_path: str):
+def main(model_path: str, model_output_path: str, batch_size: int,
+         test_dataset: str, rule_calculate: bool,
+         llama: bool, device: str, lora: bool,
+         cuda: bool, lora_path: str):
+
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.float16,
+        bnb_4bit_use_double_quant=True
+    )
     if llama:
-        model = LlamaForCausalLM.from_pretrained(model_path)
-        tokenizer = LlamaTokenizer.from_pretrained(model_path, padding_side='left')
+        model = LlamaForCausalLM.from_pretrained(
+            model_path,
+            quantization_config=bnb_config,
+            device_map="auto"
+        )
+
+        tokenizer = LlamaTokenizer.from_pretrained(
+            model_path,
+            padding_side='left'
+        )
     else:
         model = AutoModel.from_pretrained(model_path)
         tokenizer = AutoTokenizer.from_pretrained(model_path)
